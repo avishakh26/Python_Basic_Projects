1da17e47b0509d06411c2523d379d0600666f4fc
Avishakh Chakrabortty <studiouspook@gmail.com>
2026-01-17 01:49:25 +0600

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,31 @@
+print("Student's name ")
+
+name  : str  = input ("Enter your name : ")
+age   : int   = input ("ENter your age : ")
+gpa   : float = input ("Enter your GPA : ")
+father: int = input ("Enter your father's name : ")
+mother: int = input ("Enter your mother's name : ")
+data  : str  = input("Write about yourself : ")
+
+
+
+print("Student's information  ")
+print("......................")
+print("Name          = ",name)
+print("Age           = ",age)
+print("GPA           =", gpa)
+print("Father's name =", father)
+print("Mother's name =",mother)
+print("Biodata       = " ,data)
+
+
+# Testimonial on behalf of a teacher
+
+
+print (name," is a good boy I have ever seen.")
+print("When he/she was ",age," years old \n I saw him/her keep focousing on study")
+print("His/her father MR.",father, "is a very good guy. \t and his/her mother is very polite")
+print("His/Her GPA",gpa, "is a sign of his/her brillantness")
+
+
+

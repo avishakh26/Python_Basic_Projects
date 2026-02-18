407ee93fc7fb1a70e720f5103dd2b5fb3c1f63a6
Avishakh Chakrabortty <studiouspook@gmail.com>
2026-02-18 20:04:50 +0600

    Add files via upload

---
+++ task_3.4.py
@@ -0,0 +1,25 @@
+n1 = int(input("Enter your number :"))
+
+
+
+print(n1,"* 1=",(n1*1))
+
+print(n1,"* 2=",(n1*2))
+
+print(n1,"* 3=",(n1*3))
+
+print(n1,"* 4=",(n1*4))
+
+print(n1,"* 5=",(n1*5))
+
+print(n1,"* 6=",(n1*6))
+
+print(n1,"* 7=",(n1*7))
+
+print(n1,"* 8=",(n1*8))
+
+print(n1,"* 9=",(n1*9))
+
+print(n1,"* 10=",(n1*10))
+
+
